da0dabcd6942ecf64f91a46dd5b13e9cc466d25c
TBaum <tbaum@mit.edu>
2017-09-25 11:59:25 -0400

    Update

--- read_dat_files.m
+++ read_dat_files.m
@@ -2,12 +2,19 @@ clc
 close all
 clear
 
-%%% Read in EEG g.Nautilus data from Andrew's systems
+%% Read in EEG g.Nautilus data from Andrew's systems
 cd 'C:\Users\tbaum\OneDrive\Documents\Research\BCI Files\Andrew_MI_frontshift_chair001';
 filename = 'Andrew_MI_frontshift_chairS001R01.dat';
-% filepath = dir([foldername '/*' filename]);
+
 [data_eeg,B,C,D] = load_bcidat(filename,'-calibrated');
 acceleration_x = B.AccelerationX1;
 acceleration_y = B.AccelerationY1;
 acceleration_z = B.AccelerationZ1;
 stimulus = B.TargetCode;
+
+%% Filter EEG data
+number_of_channels = size(data_eeg,2);
+channels = {'Cz', 'Fz', 'P3', 'Pz', 'P4', 'PO7', 'PO8', 'Oz'}; % Channels from 9/11 AndrewBCI2000_gNautilus_MI_wheelchairS001R03.dat
+
+%% Plot Filtered Data
+
